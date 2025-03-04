8fce277faa32e041cb93bbf4bafef7b2c0efaac4
Abin Mathew <abinmathew004@gmail.com>
2025-03-04 17:45:39 -0500

    port fix

--- server.js
+++ server.js
@@ -16,4 +16,6 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/transactions", require("./routes/transactionRoutes"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`🚀 Server running on port ${PORT}`)
+);
